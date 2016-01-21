a8a4da4d0bd374b93f65eae46c43339a77db17e3
Anthony <schubeal@gmail.com>
2016-01-20 21:55:22 -0700

    Better return codes

--- compile_lang.py
+++ compile_lang.py
@@ -4,6 +4,8 @@ import os
 import tempfile
 import subprocess
 
+from subprocess import CalledProcessError
+
 class CompilerBase(object):
 
   DIR = '/tmp'
@@ -12,8 +14,8 @@ class CompilerBase(object):
       self.code = code
 
   def compile_code(self):
-      print('doing nothing')
-      pass
+      """Return standard unix return code"""
+      return 0
 
 class C_Compiler(CompilerBase):
     COMPILER = 'gcc'
@@ -23,18 +25,29 @@ class C_Compiler(CompilerBase):
         super().__init__(code)
 
     def compile_code(self):
+        ret = -1
         with tempfile.NamedTemporaryFile(suffix=C_Compiler.SUFFIX, dir=CompilerBase.DIR) as f:
             f.write(bytes(self.code, 'UTF-8'))
             f.flush()
             f.seek(0)
-            subprocess.call([C_Compiler.COMPILER, f.name])
+            try:
+                ret = subprocess.check_call([C_Compiler.COMPILER, f.name])
+            except CalledProcessError as e:
+                ret = e.returncode
+        return ret
 
 def run_compiler(lang, text):
 
     compiler = CompilerBase(text)
     if lang == 'C':
         compiler = C_Compiler(text)
-    compiler.compile_code()
+    return compiler.compile_code()
 
 if __name__ == '__main__':
-  print('we\'re good')
+    sample_c_prog = '#include "stdio.h"\nint main() { return 0;}';
+    res = run_compiler('C', sample_c_prog)
+    print('ran compiler with result {}'.format(res))
+    # Missing semi-colon after return
+    bad_c_prog = '#include "stdio.h"\nint main() { return 0}';
+    res = run_compiler('C', bad_c_prog)
+    print('ran compiler with result {}'.format(res))
