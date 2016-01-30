2e2a11d3329e837721a4de1f158b42e35005b09a
Anthony <schubeal@gmail.com>
2016-01-30 10:27:48 -0700

    Roll version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.0.2a6',
+    version='0.0.2a7',
 
     description='Library of wrappers for various compilers',
     long_description=long_description,
